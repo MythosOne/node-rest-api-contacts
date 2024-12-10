cf85459337c856c9c7d45aa92fbf1a982f5976c5
MythosOne <107143884+MythosOne@users.noreply.github.com>
2024-12-10 21:19:17 +0200

    fix res.status logout, correct schema contact swagger

--- controllers/authController.js
+++ controllers/authController.js
@@ -69,7 +69,7 @@ const logout = async (req, res, next) => {
   const { _id } = req.user;
   await User.findByIdAndUpdate(_id, { token: null });
 
-  res.status(200).send();
+  res.status(204).send();
 };
 
 module.exports = {
